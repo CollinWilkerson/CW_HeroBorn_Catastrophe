cc6e363fea83a884ae30c0b355b19893f3c219b5
CollinWilkerson <wilkerc2@my.erau.edu>
2024-01-23 14:59:12 -0700

    Player stuff
    added cinemachine for free camera
    did some computer wizardry to make the player move and follow the camera and stuff
    I hurt

--- Assets/Scripts/Player_Behavior.cs
+++ Assets/Scripts/Player_Behavior.cs
@@ -8,14 +8,21 @@ public class Player_Behavior : MonoBehaviour
     //initial variables
     public float moveSpeed = 100f;
 
+    //camera reference
+    public Transform cam;
+
     private float vInput;
     private float hInput;
+    private float targetAngle;
 
     private Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        // this gets rid of the cursor for camera movement purposes
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         _rb = GetComponent<Rigidbody>();  
     }
 
@@ -23,17 +30,28 @@ public class Player_Behavior : MonoBehaviour
     void Update()
     {
         /* i have no clue what im doing */
+        
         //sets up movement quantities for the rigidbody system
         vInput = Input.GetAxis("Vertical") * moveSpeed;
+        //Debug.Log(vInput);
         hInput = Input.GetAxis("Horizontal") * moveSpeed;
     }
 
     // use Fixedupdate for rigidbodies
     private void FixedUpdate()
     {
-        //vertical movement
-        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
-        //horizontal movement
-        _rb.MovePosition(this.transform.position + this.transform.forward * hInput * Time.fixedDeltaTime);
+        //removes everything but the y component
+        Vector3 YRot = new Vector3(0,cam.eulerAngles.y,0);
+        Quaternion QYRot = Quaternion.Euler(YRot);
+        //making rotation follow the rotation of the camera
+        _rb.MoveRotation(QYRot);
+        //Debug.Log("v3" + YRot + "Q" + QYRot.y);
+
+        //Debug.Log("rb" + _rb.rotation.eulerAngles.y);
+        //Debug.Log("cam" + cam.eulerAngles.y);
+
+        // if you try to update one position then another the rb will only operate on the last one because of the frame call, so you have to consalidate movements
+        //_rb.MovePosition(this.transform.position + this.transform.right * hInput * Time.fixedDeltaTime + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.right * hInput * Time.fixedDeltaTime + this.transform.forward * vInput * Time.fixedDeltaTime);
     }
 }
