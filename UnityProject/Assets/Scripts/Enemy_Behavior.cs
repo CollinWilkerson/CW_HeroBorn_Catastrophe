7bf10216bb7cea5f706c05afbbaecba68c2482a7
CollinWilkerson <wilkerc2@my.erau.edu>
2024-02-04 22:42:50 -0700

    added enemy

---
+++ UnityProject/Assets/Scripts/Enemy_Behavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Behavior : MonoBehaviour
+{
+    //triggers when a gameobject enters the sphere
+    void OnTriggerEnter(Collider other)
+    {
+        //checks game object name
+        if (other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
+    //triggers when gameobject leaves the sphere
+    void OnTriggerExit(Collider other)
+    {
+        //does same thing as before
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+}
