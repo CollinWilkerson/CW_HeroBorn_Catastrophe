0bb1e03609c3e89d9b092f675524b320809d3526
CollinWilkerson <wilkerc2@my.erau.edu>
2024-02-29 15:58:31 -0700

    There's a minimap

--- UnityProject/Assets/Scripts/Items/Clip_Behavior.cs
+++ UnityProject/Assets/Scripts/Items/Clip_Behavior.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Clip_Behavior : MonoBehaviour
 {
     public Player_Behavior player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Behavior>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
