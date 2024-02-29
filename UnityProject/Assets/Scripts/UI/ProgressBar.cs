d78bff81689bcd77b955b9beda43c6fe56de6d05
CollinWilkerson <wilkerc2@my.erau.edu>
2024-02-29 15:38:26 -0700

    Things are disappearing and I'm going insane

--- UnityProject/Assets/Scripts/UI/ProgressBar.cs
+++ UnityProject/Assets/Scripts/UI/ProgressBar.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    public GameObject player;
     public Enemy_Behavior enemy;
     public Image mask;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.transform.LookAt(player.transform);
         GetCurrentFill();
     }
 
